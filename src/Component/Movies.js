34bc39c52e07dfd40bebeb7778182de133d0e757
Harsh517-tech415 <83271527+Harsh517-tech415@users.noreply.github.com>
2023-03-23 19:26:51 +0530

    delete and edit

--- src/Component/Movies.js
+++ src/Component/Movies.js
@@ -1,113 +1,249 @@
-import React, { useEffect, useMemo, useState } from "react";
+import React, { useCallback,  useMemo, useState } from "react";
 import Papa from "papaparse";
-import MaterialReactTable from 'material-react-table';
-import { Box } from "@mui/material";
+import MaterialReactTable from "material-react-table";
+import {Box,Button,Dialog,DialogActions,DialogContent,DialogTitle,IconButton,MenuItem,Stack,TextField,Tooltip} from "@mui/material";
+import { Delete, Edit } from '@mui/icons-material';
 
 
 const Movies = () => {
   const [data, setData] = useState([]);
-  const [columnArray, setColumn] = useState([]);
-  const [values, setValues] = useState([]);
+  const [createModalOpen, setCreateModalOpen] = useState(false);
+  const [tableData, setTableData] = useState(() => data);
+  const [validationErrors, setValidationErrors] = useState({});
 
-  
+  const handleCreateNewRow = (values) => {
+    // data.push(values);
+    setData([...data,values]);
+  };
+
+  const handleSaveRowEdits = async ({ exitEditingMode, row, values }) => {
+    if (!Object.keys(validationErrors).length) {
+      data[row.index] = values;
+      //send/receive api updates here, then refetch or update local table data for re-render
+      setData([...data]);
+      exitEditingMode(); //required to exit editing mode and close modal
+    }
+  };
+
+  const handleCancelRowEdits = () => {
+    setValidationErrors({});
+  };
+
+  const handleDeleteRow = useCallback(
+    (row) => {
+      if (window.confirm(`Are you sure you want to delete ${row.getValue('id ','title')}`))
+        {let rowData=data;
+         rowData.splice(row.index, 1);
+          setData([...rowData]);
+      }
+    },
+  );
+
   const handleFileUpload = (event) => {
     Papa.parse(event.target.files[0], {
       header: true,
       dynamicTyping: true,
       skipEmptyLines: true,
       complete: function (result) {
-        const columnArray = [];
-        const valuesArray = [];
-        console.log(result.data);
         result.data.map((d) => {
-          columnArray.push(Object.keys(d));
-          valuesArray.push(Object.values(d));
         });
-        console.log(result.data)
         setData(result.data);
-        setColumn(columnArray[0]);
-        setValues(valuesArray);
       },
     });
   };
-  
-  const ColmunsData = useMemo(
-    () => [
-      {
-        accessorKey: 'id', //access nested data with dot notation
-        header: 'Id',
-      },
-      {
-        accessorKey: 'title',
-        header: 'Title',
-      },
-      {
-        accessorKey: 'imdbid', //normal accessorKey
-        header: 'Imdbid',
-      },
-      {
-        accessorKey: 'language',
-        header: 'Language',
-      },
-      {
-        accessorKey: 'parentalguidance',
-        header: 'Parentalguidance',
-      },
-      {
-        accessorKey: 'genre',
-        header: 'Genre',
-      }, {
-        accessorKey: 'isseries',
-        header: 'Isseries',
-      }, {
-        accessorKey: 'director',
-        header: 'Director',
-      }, {
-        accessorKey: 'country',
-        header: 'Country',
-      }, {
-        accessorKey: 'arrating',
-        header: 'Arrating',
-      }, {
-        accessorKey: 'runningtime',
-        header: 'Runningtime',
-      }, {
-        accessorKey: 'releasedate',
-        header: 'Releasedate',
-      }, {
-        accessorKey: 'imdbrating',
-        header: 'Imdbrating',
-      }, {
-        accessorKey: 'posterimage',
-        header: 'Posterimage',
-      }, {
-        accessorKey: 'backdropimage',
-        header: 'Backdropimage',
-      }, {
-        accessorKey: 'votecount',
-        header: 'Votecount',
-      }, {
-        accessorKey: 'mood',
-        header: 'Mood',
-      }, {
-        accessorKey: 'trailerkeytmdb',
-        header: 'Trailerkeytmdb',
-      }, {
-        accessorKey: 'backdropimageurl',
-        header: 'Backdropimageurl',
-      },
 
-    ],
-  );
-  
+  const ColmunsData = useMemo(() => [
+    {
+      accessorKey: "id", //access nested data with dot notation
+      header: "Id",
+      enableColumnOrdering: false,
+      enableEditing: false, //disable editing on this column
+      enableSorting: false,
+      size: 80,
+    },
+    {
+      accessorKey: "title",
+      header: "Title",
+    },
+    {
+      accessorKey: "imdbid", //normal accessorKey
+      header: "Imdbid",
+    },
+    {
+      accessorKey: "language",
+      header: "Language",
+    },
+    {
+      accessorKey: "parentalguidance",
+      header: "Parentalguidance",
+    },
+    {
+      accessorKey: "genre",
+      header: "Genre",
+    },
+    {
+      accessorKey: "isseries",
+      header: "Isseries",
+    },
+    {
+      accessorKey: "director",
+      header: "Director",
+    },
+    {
+      accessorKey: "country",
+      header: "Country",
+    },
+    {
+      accessorKey: "arrating",
+      header: "Arrating",
+    },
+    {
+      accessorKey: "runningtime",
+      header: "Runningtime",
+    },
+    {
+      accessorKey: "releasedate",
+      header: "Releasedate",
+    },
+    {
+      accessorKey: "imdbrating",
+      header: "Imdbrating",
+    },
+    {
+      accessorKey: "posterimage",
+      header: "Posterimage",
+    },
+    {
+      accessorKey: "backdropimage",
+      header: "Backdropimage",
+    },
+    {
+      accessorKey: "votecount",
+      header: "Votecount",
+    },
+    {
+      accessorKey: "mood",
+      header: "Mood",
+    },
+    {
+      accessorKey: "trailerkeytmdb",
+      header: "Trailerkeytmdb",
+    },
+    {
+      accessorKey: "backdropimageurl",
+      header: "Backdropimageurl",
+    },
+  ]);
+
   return (
     <>
       <input type="file" onChange={handleFileUpload} />
-      <Box sx={{ml:"20%"}}>
-      <MaterialReactTable columns={ColmunsData} data={data} />
+      <Box sx={{ ml: "20%" }}>
+        <MaterialReactTable
+          columns={ColmunsData}
+          data={data}
+          displayColumnDefOptions={{
+            "mrt-row-actions": {
+              muiTableHeadCellProps: {
+                align: "center",
+              },
+              size: 120,
+            },
+          }}
+          editingMode='modal' 
+          enableColumnOrdering
+          enableEditing
+          onEditingRowSave={handleSaveRowEdits}
+          onEditingRowCancel={handleCancelRowEdits}
+
+          
+          renderRowActions={({ row, table }) => (
+            <Box sx={{ display: "flex", gap: "1rem" }}>
+              <Tooltip arrow placement="left" title="Edit">
+                <IconButton onClick={() => table.setEditingRow(row)}>
+                  <Edit />
+                </IconButton>
+              </Tooltip>
+              <Tooltip arrow placement="right" title="Delete">
+                <IconButton color="error" onClick={() => handleDeleteRow(row)}>
+                  <Delete />
+                </IconButton>
+              </Tooltip>
+            </Box>
+          )}
+          renderTopToolbarCustomActions={() => (
+            <Button
+              color='secondary'
+              onClick={() => setCreateModalOpen(true)}
+              variant="contained"
+            >
+              Create New Account
+            </Button>
+          )}
+        />
+        <CreateNewAccountModal
+          columns={ColmunsData}
+          open={createModalOpen}
+          onClose={() => setCreateModalOpen(false)}
+          onSubmit={handleCreateNewRow}
+        />
       </Box>
     </>
   );
 };
+
+
+
+
+export const CreateNewAccountModal = ({ open, columns, onClose, onSubmit }) => {
+  const [values, setValues] = useState(() =>
+    columns.reduce((acc, column) => {
+      acc[column.accessorKey ?? ""] = "";
+      return acc;
+    }, {})
+  );
+
+  const handleSubmit = () => {
+    //put your validation logic here
+    console.log(values)
+    onSubmit(values);
+    onClose();
+  };
+
+  return (
+    <Dialog open={open}>
+      <DialogTitle textAlign="center">Create New Account</DialogTitle>
+      <DialogContent>
+        <form onSubmit={(e) => e.preventDefault()}>
+          <Stack
+            sx={{
+              width: "100%",
+              minWidth: { xs: "300px", sm: "360px", md: "400px" },
+              gap: "1.5rem",
+            }}
+          >
+            {columns.map((column) => (
+              <TextField
+                key={column.accessorKey}
+                label={column.header}
+                name={column.accessorKey}
+                onChange={(e) =>
+                  setValues({ ...values, [e.target.name]: e.target.value })
+                }
+              />
+            ))}
+          </Stack>
+        </form>
+      </DialogContent>
+      <DialogActions sx={{ p: "1.25rem" }}>
+        <Button onClick={onClose}>Cancel</Button>
+        <Button color="secondary" onClick={handleSubmit} variant="contained">
+          Create New Account
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
+};
+
 
 export default Movies;
