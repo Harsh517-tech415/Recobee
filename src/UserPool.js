bca29def471402b8de0418c209183b5c096f1fc0
Harsh517-tech415 <83271527+Harsh517-tech415@users.noreply.github.com>
2023-04-03 13:43:12 +0530

    remove .env file from git

--- src/UserPool.js
+++ src/UserPool.js
@@ -1,6 +1,6 @@
 import {CognitoUserPool} from 'amazon-cognito-identity-js';
 const poolData={
-    UserPoolId:process.env.REACT_APP_AWS_COGNITO_USER_POOL_ID,
-    ClientId:process.env.REACT_APP_AWS_COGNITO_CLIENT_ID
+    UserPoolId: process.env.REACT_APP_AWS_COGNITO_USER_POOL_ID,
+  ClientId: process.env.REACT_APP_AWS_COGNITO_CLIENT_ID
 }
 export default new CognitoUserPool(poolData);
